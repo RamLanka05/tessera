b13086611f9c12445f2686e6ba54c4e66d9d04c8
RamLanka05 <ramlanka1208@gmail.com>
2026-08-16 20:28:36 -0500

    added files necessary for raft, nothing in them yet

---
+++ backend/internal/raft/storage.go
@@ -0,0 +1,4 @@
+// Future implementation of a persistent storage layer for Raft consensus algorithm will go here.
+// This will handle the persistence of log entries, snapshots, and other state necessary for Raft to function correctly.
+
+package raft
